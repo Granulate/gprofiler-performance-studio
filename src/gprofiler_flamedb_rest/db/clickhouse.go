667eb1fdbe6ab1debb0c8ca3238e52b0382d544c
Inbar Svarzenberg <inbar.svarzenberg@granulate.io>
2024-01-09 14:11:42 +0200

    fix

--- src/gprofiler_flamedb_rest/db/clickhouse.go
+++ src/gprofiler_flamedb_rest/db/clickhouse.go
@@ -263,11 +263,11 @@ func NewClickHouseClient(addr string) *ClickHouseClient {
 	}
 	if err := db.Ping(); err != nil {
 		if exception, ok := err.(*clickhouse.Exception); ok {
-			log.Debug("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+			log.Fatalf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
 		} else {
-			log.Debug(err)
+		    log.Fatal(err)
 		}
-		log.Fatalf("ping failed")
+		    log.Fatalf("ping failed")
 		return nil
 	}
 	return &ClickHouseClient{
