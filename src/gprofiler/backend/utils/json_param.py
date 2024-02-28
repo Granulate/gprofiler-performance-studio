c9755cfb4c227577f00fde3c61f39a00a570d719
Motti Saroka <motti.saroka@granulate.io>
2024-02-28 14:40:02 +0200

    Remove the old license comment

--- src/gprofiler/backend/utils/json_param.py
+++ src/gprofiler/backend/utils/json_param.py
@@ -1,7 +1,4 @@
-# INTEL CONFIDENTIAL
-# Copyright (C) 2023 Intel Corporation
-# This software and the related documents are Intel copyrighted materials, and your use of them is governed by the express license under which they were provided to you ("License"). Unless the License provides otherwise, you may not use, modify, copy, publish, distribute, disclose or transmit this software or the related documents without Intel's prior written permission.
-# This software and the related documents are provided as is, with no express or implied warranties, other than those that are expressly stated in the License.
+
 
 # source: https://gist.github.com/imankulov/cef71dd5a01f9a27caeb66f7bedaf241
 # this module is used due to the fact that FastAPI doesn't support json query validation against model
